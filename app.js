07df970dd79b6cc98cd9b4c5ebdfa6852839f7af
GCMoura <51333439+GCMoura@users.noreply.github.com>
2020-02-16 09:58:32 -0300

    16/02

--- app.js
+++ app.js
@@ -1,74 +1,69 @@
 var aleatoryNumber = Math.floor(Math.random() * 100) + 1;
-var answer = document.querySelector('.answer');
-var number = document.getElementById('number');
-var space = document.getElementById('last-hint');
-var button = document.querySelector('#hint');
+var answer = document.querySelector(".answer");
+var count = document.querySelector(".count");
+var number = document.getElementById("number");
+var space = document.getElementById("last-hint");
+var button = document.querySelector(".hintBtn");
 var guessCount = 0;
 
 number.focus();
 
-
 function hint() {
-
-    space.innerHTML += ` ${number.value} `
+    space.innerHTML += ` ${number.value} `;
     number.focus();
 
     validation();
-  
-    number.value = '';
+
+    number.value = "";
 }
 
-
 function validation() {
-
     if (number.value == aleatoryNumber) {
         endGame();
     } else if (number.value < aleatoryNumber) {
-        answer.innerHTML = `Número menor!`
-        answer.style.backgroundColor = 'red';
-        guessCount += 1
-        guess()
+        answer.innerHTML = `${number.value} é menor. Digite um número maior.`;
+        answer.style.backgroundColor = "red";
+        guessCount += 1;
+        count.innerHTML = `Jogada número: ${guessCount}`;
+        count.style.backgroundColor = "blue";
+        guess();
     } else if (number.value > aleatoryNumber) {
-        answer.innerHTML = `Número maior!`
-        answer.style.backgroundColor = 'red';
-        guessCount += 1
-        guess()
-    } 
-
+        answer.innerHTML = `${number.value} é maior. Digite um número menor.`;
+        answer.style.backgroundColor = "red";
+        guessCount += 1;
+        count.innerHTML = `Jogada número: ${guessCount}`;
+        count.style.backgroundColor = "blue";
+        guess();
+    }
 }
 
 function guess() {
-
     if (guessCount === 10) {
         gameError();
-    } 
+    }
 }
 
 function gameError() {
-    
-    answer.innerHTML = `Suas chances acabaram!`
-    answer.style.backgroundColor = 'red';   
-    number.value = '';
+    answer.innerHTML = `Suas chances acabaram!`;
+    answer.style.backgroundColor = "red";
+    count.innerHTML = `Tente de novo!`;
+    count.style.backgroundColor = "blue";
+    number.value = "";
     button.disabled = true;
-
 }
 
 function endGame() {
-
-    answer.innerHTML = `Acertou`
-    answer.style.backgroundColor = 'green';    
-    number.value = '';
+    answer.innerHTML = `Acertou`;
+    answer.style.backgroundColor = "green";
+    number.value = "";
     button.disabled = true;
-    
 }
 
 function newGame() {
-    
-    answer.innerHTML = '';
-    guessCount = 0
-    space.innerHTML = '';
+    count.innerHTML = "";
+    answer.innerHTML = "";
+    guessCount = 0;
+    space.innerHTML = "";
     aleatoryNumber = Math.floor(Math.random() * 100) + 1;
-    button.disabled = false;   
-   
+    button.disabled = false;
 }
- 
